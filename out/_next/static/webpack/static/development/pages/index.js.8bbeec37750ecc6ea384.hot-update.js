2a4dff1a2b4b09fe1f123f35bd2ce1cb4f310030
benbousquet <bbousque@asu.edu>
2019-11-10 03:13:56 -0700

    remove useless files

---
+++ out/_next/static/webpack/static/development/pages/index.js.8bbeec37750ecc6ea384.hot-update.js
@@ -0,0 +1,149 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./components/home/creditCard/index.js":
+/*!*********************************************!*\
+  !*** ./components/home/creditCard/index.js ***!
+  \*********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./style.css */ "./components/home/creditCard/style.css");
+/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_style_css__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "C:\\dev\\node_projects\\breadit\\components\\home\\creditCard\\index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return __jsx("div", {
+    "class": "card",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, __jsx("div", {
+    "class": "card__front card__part",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, __jsx("img", {
+    "class": "card__front-square card__square",
+    src: "https://image.ibb.co/cZeFjx/little_square.png",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }), __jsx("p", {
+    "class": "card_numer",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, "**** **** **** 6258"), __jsx("div", {
+    "class": "card__space-75",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, __jsx("span", {
+    "class": "card__label",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, "Card holder"), __jsx("p", {
+    "class": "card__info",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, "John Doe")), __jsx("div", {
+    "class": "card__space-25",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }, __jsx("span", {
+    "class": "card__label",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, "Expires"), __jsx("p", {
+    "class": "card__info",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }, "10/25"))), __jsx("div", {
+    "class": "card__back card__part",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    },
+    __self: this
+  }, __jsx("div", {
+    "class": "card__black-line",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 22
+    },
+    __self: this
+  }), __jsx("div", {
+    "class": "card__back-content",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 23
+    },
+    __self: this
+  }, __jsx("div", {
+    "class": "card__secret",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 24
+    },
+    __self: this
+  }, __jsx("p", {
+    "class": "card__secret--last",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 25
+    },
+    __self: this
+  }, "420")), __jsx("img", {
+    "class": "card__back-square card__square",
+    src: "https://image.ibb.co/cZeFjx/little_square.png",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27
+    },
+    __self: this
+  }), __jsx("img", {
+    "class": "card__back-logo card__logo",
+    src: "https://www.fireeye.com/partners/strategic-technology-partners/visa-fireeye-cyber-watch-program/_jcr_content/content-par/grid_20_80_full/grid-20-left/image.img.png/1505254557388.png",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 31
+    },
+    __self: this
+  }))));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.8bbeec37750ecc6ea384.hot-update.js.map
---
+++ out/_next/static/webpack/static/development/pages/index.js.adb0d8740ddf4f756324.hot-update.js
@@ -0,0 +1,82 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./components/home/cardShowcase.js":
+/*!*****************************************!*\
+  !*** ./components/home/cardShowcase.js ***!
+  \*****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var antd_lib_typography_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! antd/lib/typography/style/css */ "./node_modules/antd/lib/typography/style/css.js");
+/* harmony import */ var antd_lib_typography_style_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(antd_lib_typography_style_css__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var antd_lib_typography__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! antd/lib/typography */ "./node_modules/antd/lib/typography/index.js");
+/* harmony import */ var antd_lib_typography__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(antd_lib_typography__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _creditCard_index__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./creditCard/index */ "./components/home/creditCard/index.js");
+/* harmony import */ var react_tilt__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react-tilt */ "./node_modules/react-tilt/dist/tilt.js");
+/* harmony import */ var react_tilt__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(react_tilt__WEBPACK_IMPORTED_MODULE_4__);
+
+
+var _jsxFileName = "C:\\dev\\node_projects\\breadit\\components\\home\\cardShowcase.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement;
+
+
+var Title = antd_lib_typography__WEBPACK_IMPORTED_MODULE_1___default.a.Title;
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, __jsx(Title, {
+    style: {
+      textAlign: "center",
+      paddingTop: "20px"
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, "Introducing the Breadit Card"), __jsx(react_tilt__WEBPACK_IMPORTED_MODULE_4___default.a, {
+    className: "Tilt",
+    options: {
+      max: 30
+    },
+    style: {
+      margin: "auto",
+      height: 190,
+      width: 320,
+      display: "flex",
+      justifySelf: "center"
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, __jsx("div", {
+    className: "Tilt-inner",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 22
+    },
+    __self: this
+  }, __jsx(_creditCard_index__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 23
+    },
+    __self: this
+  }))));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.adb0d8740ddf4f756324.hot-update.js.map
